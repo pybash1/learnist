79d7761a2f6b3c8047a2389d8998d6565dd4c62f
pybash1 <example@example.com>
2022-06-29 22:15:37 +0530

    fix(jsx): fix jsx rules

--- components/CTA.jsx
+++ components/CTA.jsx
@@ -1,31 +1,31 @@
 export default function CTA() {
   return (
-    <aside class="p-12 bg-primary sm:p-16 lg:p-24">
-      <div class="max-w-xl mx-auto text-center">
-        <p class="text-sm font-medium text-gray-300">
-          Don't get overwhelmed with your studies!
+    <aside className="p-12 bg-primary sm:p-16 lg:p-24">
+      <div className="max-w-xl mx-auto text-center">
+        <p className="text-sm font-medium text-gray-300">
+          Don&apos;t get overwhelmed with your studies!
         </p>
 
-        <p class="mt-2 text-3xl font-bold text-white sm:text-4xl">
+        <p className="mt-2 text-3xl font-bold text-white sm:text-4xl">
           Ace them with us!
         </p>
 
-        <form class="mt-8 sm:flex">
-          <div class="sm:flex-1">
-            <label for="email" class="sr-only">
+        <form className="mt-8 sm:flex">
+          <div className="sm:flex-1">
+            <label htmlFor="email" className="sr-only">
               Email
             </label>
 
             <input
               type="email"
               placeholder="john.doe@yourschool.edu"
-              class="w-full p-3 text-white bg-gray-800 border-2 border-gray-700 rounded-lg"
+              className="w-full p-3 text-white bg-gray-800 border-2 border-gray-700 rounded-lg"
             />
           </div>
 
           <button
             type="submit"
-            class="flex items-center justify-between w-full px-5 py-3 mt-4 font-medium text-white bg-accent rounded-lg sm:w-auto sm:mt-0 sm:ml-4 hover:bg-accent/75 transition ease-in-out"
+            className="flex items-center justify-between w-full px-5 py-3 mt-4 font-medium text-white bg-accent rounded-lg sm:w-auto sm:mt-0 sm:ml-4 hover:bg-accent/75 transition ease-in-out"
           >
             Sign up
             <svg
@@ -33,12 +33,12 @@ export default function CTA() {
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
-              class="flex-shrink-0 w-4 h-4 ml-3"
+              className="flex-shrink-0 w-4 h-4 ml-3"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M14 5l7 7m0 0l-7 7m7-7H3"
               />
             </svg>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,53 +2,53 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <header class="shadow-sm bg-neutral">
-      <div class="max-w-screen-xl p-4 mx-auto">
-        <div class="flex items-center justify-between space-x-4 lg:space-x-10">
-          <div class="flex lg:w-0 lg:flex-1">
+    <header className="shadow-sm bg-neutral">
+      <div className="max-w-screen-xl p-4 mx-auto">
+        <div className="flex items-center justify-between space-x-4 lg:space-x-10">
+          <div className="flex lg:w-0 lg:flex-1">
             <div className="text-accent">LEARNIST</div>
           </div>
 
-          <nav class="hidden space-x-8 text-sm font-medium md:flex">
-            <a class="text-white hover:text-accent transition ease-in-out" href="">
+          <nav className="hidden space-x-8 text-sm font-medium md:flex">
+            <a className="text-white hover:text-accent transition ease-in-out" href="">
               About
             </a>
-            <a class="text-white hover:text-accent transition ease-in-out" href="">
+            <a className="text-white hover:text-accent transition ease-in-out" href="">
               Blog
             </a>
-            <a class="text-white hover:text-accent transition ease-in-out" href="">
+            <a className="text-white hover:text-accent transition ease-in-out" href="">
               Projects
             </a>
-            <a class="text-white hover:text-accent transition ease-in-out" href="">
+            <a className="text-white hover:text-accent transition ease-in-out" href="">
               Contact
             </a>
           </nav>
 
-          <div class="items-center justify-end flex-1 hidden space-x-4 sm:flex">
+          <div className="items-center justify-end flex-1 hidden space-x-4 sm:flex">
             <a
-              class="px-5 py-2 text-sm font-medium text-white bg-primary hover:bg-gray-500 transition ease-in-out rounded-lg"
+              className="px-5 py-2 text-sm font-medium text-white bg-primary hover:bg-gray-500 transition ease-in-out rounded-lg"
               href=""
             >
               Log in
             </a>
 
             <a
-              class="px-5 py-2 text-sm font-medium text-white bg-accent hover:bg-accent/75 transition ease-in-out rounded-lg"
+              className="px-5 py-2 text-sm font-medium text-white bg-accent hover:bg-accent/75 transition ease-in-out rounded-lg"
               href=""
             >
               Sign up
             </a>
           </div>
 
-          <div class="lg:hidden">
+          <div className="lg:hidden">
             <button
-              class="p-2 text-gray-600 bg-gray-100 rounded-lg"
+              className="p-2 text-gray-600 bg-gray-100 rounded-lg"
               type="button"
             >
-              <span class="sr-only">Open menu</span>
+              <span className="sr-only">Open menu</span>
               <svg
                 aria-hidden="true"
-                class="w-5 h-5"
+                className="w-5 h-5"
                 fill="none"
                 stroke="currentColor"
                 viewbox="0 0 24 24"
@@ -56,9 +56,9 @@ export default function Navbar() {
               >
                 <path
                   d="M4 6h16M4 12h16M4 18h16"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                 />
               </svg>
             </button>
