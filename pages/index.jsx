a2bb379574741a96e370c9efff28eb465c805ac0
pybash1 <example@example.com>
2022-07-01 09:34:55 +0530

    feat(reviews): added testimonials and form to add new testimonials

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import CTA from "../components/CTA";
 import Features from "../components/Features";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import Testimonials from "../components/Testimonials";
 
 export default function Home() {
   return (
@@ -10,7 +11,18 @@ export default function Home() {
       <CTA />
       <br />
       <Features />
+      <Testimonials />
+      <div className="flex flex-col items-center">
+        <a
+          className="inline-block px-12 py-3 text-sm font-medium text-white bg-accent border border-accent rounded active:text-indigo-500 hover:bg-accent/75 hover:border-accent/75 focus:outline-none focus:ring"
+          href="https://tally.so/r/waQGpE"
+          target="blank_"
+          rel="noreferrer noopener"
+        >
+          Add Your Testimonial
+        </a>
+      </div>
       <Footer />
     </div>
-  )
+  );
 }
