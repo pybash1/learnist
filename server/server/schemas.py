1993e96637a0ac789451354b050fe3bf76ba7f3e
pybash1 <67195650+ax-a-dev@users.noreply.github.com>
2022-09-12 21:09:20 +0530

    feat(schemas): new schemas for deta

---
+++ server/server/schemas.py
@@ -0,0 +1,23 @@
+from datetime import date, time
+from odetam import DetaModel
+
+
+class LearnistUser(DetaModel):
+    email: str
+    password: str
+
+
+class LearnistHomework(DetaModel):
+    name: str
+    description: str
+    due: date
+    class_: str
+    assigned_by: str
+
+
+class LearnistClass(DetaModel):
+    name: str
+    teacher: str
+    day: str
+    stime: time
+    etime: time
